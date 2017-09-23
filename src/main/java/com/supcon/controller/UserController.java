7b571cf8f0a755c02cc594c378c54892ef26e0b9
xuxiaoyang <Aa123456789>
2017-09-23 22:16:43 +0800

    添加kendo ui window窗口的基本用法

--- src/main/java/com/supcon/controller/UserController.java
+++ src/main/java/com/supcon/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * 进入主页面跳转页面
  * Created by Mxia on 2017/7/13.
  */
 @Controller
@@ -23,8 +24,16 @@ public class UserController {
     public String input(ModelMap modelMap, Model model){
         model.addAttribute("address","长安");
         modelMap.put("name" ,"hello velocity");
+
+
         //return "wdatepicker/wdatepicker";//日期选择器的使用
-//        return "date/date"; //日期字符串和日期的转换和使用
-        return "ajax/serialize";//表单的序列还
+        //return "date/date"; //日期字符串和日期的转换和使用
+        //return "ajax/serialize";//表单的序列还
+        return "kendo/window";//kendowindow测试页面
+    }
+    @RequestMapping("/test")
+    public String test(){
+
+        return "wdatepicker/wdatepicker";
     }
 }
