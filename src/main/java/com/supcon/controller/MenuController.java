9611597234556e5d93f9e9e7ff6a6f1ac8737cdf
xuxiaoyang <Aa123456789>
2017-09-26 15:36:44 +0800

    添加bootstrap框架 美化显示页面

---
+++ src/main/java/com/supcon/controller/MenuController.java
@@ -0,0 +1,109 @@
+package com.supcon.controller;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 进入主页面跳转页面
+ * Created by Mxia on 2017/7/13.
+ */
+@Controller
+@RequestMapping("/content")
+public class MenuController {
+
+
+    /**
+     * Created by Intellij IDEA
+     * @author:xuxiaoyang
+     * @Date: 2017/9/26 14:57
+     * @Description:表单在ajax中的序列化
+     * @return
+     */
+    @RequestMapping("/ajax")
+    public String serialize(){
+        return "ajax/serialize";//表单的序列还
+    }
+
+    /**
+     * Created by Intellij IDEA
+     * @author:xuxiaoyang
+     * @Date: 2017/9/26 14:57
+     * @Description:日期字符串和日期的使用测试
+     * @return
+     */
+    @RequestMapping("/date")
+    public String date(){
+        return "date/date"; //日期字符串和日期的转换和使用
+    }
+
+    /**
+     * Created by Intellij IDEA
+     * @author:xuxiaoyang
+     * @Date: 2017/9/26 14:56
+     * @Description:日期选择器的使用
+     * @return
+     */
+    @RequestMapping("/wdatepicker")
+    public String wdatepicker(){
+        return "wdatepicker/wdatepicker";
+    }
+
+    /**
+     * Created by Intellij IDEA
+     * @author:xuxiaoyang
+     * @Date: 2017/9/26 14:56
+     * @Description:kendo中window对象的使用
+     * @return
+     */
+    @RequestMapping("/window")
+    public String window(){
+        return "kendo/window";
+    }
+
+    /**
+     * Created by Intellij IDEA
+     * @author:xuxiaoyang
+     * @Date: 2017/9/26 14:56
+     * @Description: array对像的测试页面
+     * @return
+     */
+    @RequestMapping("/array")
+    public String array(){
+        return "javascript/array";
+    }
+
+    /**
+     * Created by Intellij IDEA
+     * @author:xuxiaoyang
+     * @Date: 2017/9/26 14:55
+     * @Description:   滚动动画的测试使用
+     * @return
+     */
+    @RequestMapping("/flexslider")
+    public String flexslicer(){
+        return "jqueryFlexSlider/flexslider";
+    }
+
+    @RequestMapping("/user")
+    public String input(ModelMap modelMap, Model model){
+        model.addAttribute("address","长安");
+        modelMap.put("name" ,"hello velocity");
+
+
+        //return "wdatepicker/wdatepicker";//日期选择器的使用
+        //return "date/date"; //日期字符串和日期的转换和使用
+        //return "ajax/serialize";//表单的序列还
+        //return "kendo/window";//kendowindow测试页面
+        //return "javascript/array";//javascript测试页面
+        //return "jqueryFlexSlider/flexslider";
+        return "hello";
+    }
+    @RequestMapping("/test")
+    public String test(){
+
+        return "wdatepicker/wdatepicker";
+    }
+}
--- src/main/java/com/supcon/controller/WelcomeController.java
+++ src/main/java/com/supcon/controller/WelcomeController.java
@@ -1,5 +1,8 @@
 package com.supcon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Intellij IDEA
  *
@@ -8,5 +11,15 @@ package com.supcon.controller;
  * @Description: 首页 欢迎页
  */
 
+@Controller
 public class WelcomeController {
+
+    @RequestMapping("/")
+    public String login(){
+        return "redirect:menu";//重定向到 首页路径
+    }
+    @RequestMapping("/menu")
+    public String menu(){
+        return "hello";//跳转到首页
+    }
 }
